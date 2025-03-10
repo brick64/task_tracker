6dfdd1f0bb04fb7d52ea7f36a72f892bb10c83dd
Armas <nikitamitt8@gmail.com>
2025-03-10 13:45:00 +0300

    Rewrite parser initialisation

--- src/main.py
+++ src/main.py
@@ -2,43 +2,78 @@ import argparse
 
 
 def main() -> None:
+    commands = {
+        "add": {
+            "help": "Add new task",
+            "description": {
+                "type": str,
+                "help": "A short description of the task",
+            },
+        },
+        "update": {
+            "help": "Udate existing task",
+            "id": {"type": int, "help": "A unique identifier for the task"},
+            "description": {
+                "type": str,
+                "help": "A short description of the task",
+            },
+        },
+        "delete": {
+            "help": "Delete task",
+            "id": {"type": int, "help": "A unique identifier for the task"},
+        },
+        "mark-in-progress": {
+            "help": "Mark task in progress",
+            "id": {"type": int, "help": "A unique identifier for the task"},
+        },
+        "mark-done": {
+            "help": "Mark task done",
+            "id": {"type": int, "help": "A unique identifier for the task"},
+        },
+        "list": {
+            "help": "List existing tasks",
+            "status": {
+                "type": str,
+                "choises": ["done", "todo", "in-progress"],
+                "default": "",
+                "help": "The status of the task",
+            },
+        },
+    }
     parser = argparse.ArgumentParser(
         description="A simple task tracker CLI written in Python"
     )
     subparsers = parser.add_subparsers(dest="command")
 
-    add = subparsers.add_parser("add", help="Add new task")
-    add.add_argument(
-        "description", type=str, help="A short description of the task", required=True
-    )
-
-    update = subparsers.add_parser("update", help="Udate existing task")
-    update.add_argument("id", type=int, help="A unique identifier for the task")
-    update.add_argument(
-        "description", type=str, help="A short description of the task", required=True
-    )
-
-    delete = subparsers.add_parser("delete", help="Delete task")
-    delete.add_argument("id", type=int, help="A unique identifier for the task")
-
-    mark_in_progreess = subparsers.add_parser(
-        "mark-in-progress", help="Mark task in progress"
-    )
-    mark_in_progreess.add_argument(
-        "id", type=int, help="A unique identifier for the task"
-    )
-
-    mark_done = subparsers.add_parser("mark-done", help="Mark task done")
-    mark_done.add_argument("id", type=int, help="A unique identifier for the task")
-
-    list = subparsers.add_parser("list", help="List existing tasks")
-    list.add_argument(
-        "status",
-        type=str,
-        choices=["done", "todo", "in-progress"],
-        default="",
-        help="The status of the task",
-    )
+    for command in commands:
+        parser_details = commands[command]
+        help = parser_details.pop("help")
+        subparser = subparsers.add_parser(command, help=help)
+        for arg in parser_details:
+            arg_details = parser_details[arg]
+            type = arg_details.pop("type")
+            help = arg_details.pop("help")
+            choises = arg_details.pop("choises") if "choises" in arg_details else None
+            default = arg_details.pop("default") if "default" in arg_details else None
+            subparser.add_argument(
+                arg, type=type, choices=choises, default=default, help=help
+            )
+    args = parser.parse_args()
+    match args.command:
+        case "add":
+            print("add")
+        case "update":
+            print("update")
+        case "delete":
+            print("delete")
+        case "mark-in-progress":
+            print("mark-in-progress")
+        case "mark-done":
+            print("mark-done")
+        case "list":
+            print("list")
+        case _:
+            print("unknown")
 
 
 if __name__ == "__main__":
